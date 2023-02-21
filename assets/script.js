f36301064ae28637659debf2dcef867b6dee778e
William Combee <william.combee@yahoo.com>
2023-02-21 12:46:22 -0600

    added start message and button

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,14 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 
+// Timer function
 function countdown() {
     var timeLeft = 5;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+
     var timeInterval = setInterval(function () {
       // As long as the `timeLeft` is greater than 1
       if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
         timerEl.textContent = timeLeft + ' seconds remaining to complete Quiz';
-        // Decrement `timeLeft` by 1
         timeLeft--;
       } else if (timeLeft === 1) {
         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
@@ -19,12 +17,8 @@ function countdown() {
       } else {
         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
         timerEl.textContent = 'Time Up!';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayMessage();
       }
     }, 1000);
   }
+    countdown();
 
-  countdown();
